4a8e83fed8031d57d3391db729c998a0d6a8697e
Nikolay Vasilchuk <anonym.tsk@gmail.com>
2024-08-09 15:45:42 +0300

    netinstall/uninstall

---
+++ netuninstall.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+ABSOLUTE_FILENAME=`readlink -f "$0"`
+HOME_FOLDER=`dirname "$ABSOLUTE_FILENAME"`
+BASE_URL="https://github.com/Anonym-tsk/nfqws-keenetic/raw/master"
+
+cd /tmp
+mkdir -p nfqws-keenetic/common
+
+curl -SL# "$BASE_URL/uninstall.sh" -o "nfqws-keenetic/uninstall.sh"
+curl -SL# "$BASE_URL/common/install_func.sh" -o "nfqws-keenetic/common/install_func.sh"
+
+chmod +x ./nfqws-keenetic/*.sh
+./nfqws-keenetic/uninstall.sh
+
+rm -rf nfqws-keenetic
+cd $HOME_FOLDER
+
+exit 0
