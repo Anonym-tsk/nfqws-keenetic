51639120485ade10056c5b2b87bd131a962d7191
Nikolay Vasilchuk <anonym.tsk@gmail.com>
2024-08-12 12:31:12 +0300

    IPv6 rules added

--- etc/ndm/netfilter.d/100-nfqws.sh
+++ etc/ndm/netfilter.d/100-nfqws.sh
@@ -5,7 +5,7 @@
 if [ ! -f "$PIDFILE" ] || ! kill -0 $(cat "$PIDFILE"); then
   exit
 fi
-[ "$type" == "ip6tables" ] && exit
 [ "$table" != "mangle" ] && exit
 
-/opt/etc/init.d/S51nfqws firewall
+# $type is `iptables` or `ip6tables`
+/opt/etc/init.d/S51tpws firewall-"$type"
