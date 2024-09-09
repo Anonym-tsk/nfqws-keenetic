33b1055d4201e20ccf6cb946894662698bde72ae
Nikolay Vasilchuk <anonym.tsk@gmail.com>
2024-09-09 15:50:16 +0300

    Fix #35

--- netuninstall.sh
+++ netuninstall.sh
@@ -5,10 +5,11 @@ HOME_FOLDER=`dirname "$ABSOLUTE_FILENAME"`
 BASE_URL="https://raw.githubusercontent.com/Anonym-tsk/nfqws-keenetic/master"
 
 cd /opt/tmp
-mkdir -p nfqws-keenetic/common
+mkdir -p nfqws-keenetic/common/ipk
 
 curl -SL# "$BASE_URL/uninstall.sh" -o "nfqws-keenetic/uninstall.sh"
 curl -SL# "$BASE_URL/common/install_func.sh" -o "nfqws-keenetic/common/install_func.sh"
+curl -SL# "$BASE_URL/common/ipk/common" -o "nfqws-keenetic/common/ipk/common"
 
 chmod +x ./nfqws-keenetic/*.sh
 ./nfqws-keenetic/uninstall.sh
