44d25a3474e90979cfa867caec332c58dc0627c8
Nikolay Vasilchuk <anonym.tsk@gmail.com>
2024-09-09 14:34:52 +0300

    Install scripts

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,7 @@
 ABSOLUTE_FILENAME=`readlink -f "$0"`
 HOME_FOLDER=`dirname "$ABSOLUTE_FILENAME"`
 
+source $HOME_FOLDER/common/ipk/common
 source $HOME_FOLDER/common/install_func.sh
 
 # Start uninstallation
@@ -11,8 +12,8 @@ begin_uninstall_func
 # Stop service if exist
 stop_func
 
-# Remove all data
-remove_all_files_func
+# Remove main data
+remove_main_files_func
 
 # Remove lists
 remove_list_func
