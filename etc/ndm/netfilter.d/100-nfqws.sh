2d879aae36e49bbbccb4558db1e6b659913de21e
Nikolay Vasilchuk <anonym.tsk@gmail.com>
2024-09-10 18:03:06 +0300

    Paths fix && version rollback

--- etc/ndm/netfilter.d/100-nfqws.sh
+++ etc/ndm/netfilter.d/100-nfqws.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-. /opt/etc/nfqws/nfqws.conf
+ROOT_DIR=$(readlink -f $(dirname $0)/../../../)
+STARTUP_SCRIPT="$ROOT_DIR/etc/init.d/S51nfqws"
+PIDFILE="$ROOT_DIR/var/run/nfqws.pid"
+CONFFILE="$ROOT_DIR/etc/nfqws/nfqws.conf"
+
+source "$CONFFILE"
 
 if [ ! -f "$PIDFILE" ] || ! kill -0 $(cat "$PIDFILE"); then
   exit
@@ -8,4 +13,4 @@ fi
 [ "$table" != "mangle" ] && exit
 
 # $type is `iptables` or `ip6tables`
-/opt/etc/init.d/S51nfqws firewall-"$type"
+$STARTUP_SCRIPT firewall-"$type"
