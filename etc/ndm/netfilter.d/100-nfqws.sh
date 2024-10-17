7ac53e6e6da887b4720e050d3b83ff29b46a335b
Nikolay Vasilchuk <anonym.tsk@gmail.com>
2024-10-17 10:19:20 +0300

    Fixed ndm script for masquerade iptables rule (Fix #94)

--- etc/ndm/netfilter.d/100-nfqws.sh
+++ etc/ndm/netfilter.d/100-nfqws.sh
@@ -4,9 +4,7 @@ PIDFILE="/opt/var/run/nfqws.pid"
 if [ ! -f "$PIDFILE" ] || ! kill -0 $(cat "$PIDFILE") 2>/dev/null; then
   exit
 fi
-[ "$table" != "mangle" ] && exit
-
-. /opt/etc/nfqws/nfqws.conf
+[ "$table" != "mangle" ] && [ "$table" != "nat" ] && exit
 
 # $type is `iptables` or `ip6tables`
 /opt/etc/init.d/S51nfqws firewall_"$type"
