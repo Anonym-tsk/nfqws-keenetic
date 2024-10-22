7b98276d7664b0aa469714624d31e0f35515e2da
Nikolay Vasilchuk <anonym.tsk@gmail.com>
2024-10-22 17:50:44 +0300

    version added to web

--- web/share/www/nfqws/script.js
+++ web/share/www/nfqws/script.js
@@ -204,6 +204,37 @@ class UI {
                 }
             };
         })(this);
+
+        this.version = ((ui) => {
+            const element = document.getElementById('version');
+            const match = element.textContent.match(/^v([0-9]+)\.([0-9]+)\.([0-9]+)$/);
+
+            return {
+                get value() {
+                    return match ? [match[1], match[2], match[3]] : null;
+                },
+                async checkUpdate() {
+                    if (!this.value) {
+                        return;
+                    }
+
+                    const latest = await getLatestVersion();
+                    if (!latest) {
+                        return;
+                    }
+
+                    const updateAvailable = compareVersions(this.value, latest);
+                    if (updateAvailable) {
+                        const link = document.createElement('a');
+                        const tag = `v${latest[0]}.${latest[1]}.${latest[2]}`;
+                        link.textContent = `(${tag})`;
+                        link.href = `https://github.com/Anonym-tsk/nfqws-keenetic/releases/tag/${tag}`;
+                        link.target = '_blank';
+                        element.appendChild(link);
+                    }
+                }
+            }
+        })(this);
     }
 
     async loadFile(filename) {
@@ -297,8 +328,31 @@ async function restartNfqws() {
     return _postData({cmd: 'restart'});
 }
 
+async function getLatestVersion() {
+    try {
+        const response = await fetch('https://api.github.com/repos/Anonym-tsk/nfqws-keenetic/releases/latest');
+        const data = await response.json();
+        const tag = data.tag_name;
+        const match = tag.match(/^v([0-9]+)\.([0-9]+)\.([0-9]+)$/);
+        return [match[1], match[2], match[3]];
+    } catch (e) {
+        return null;
+    }
+}
+
+function compareVersions(current, latest) {
+    const v1 = latest[0] - current[0];
+    const v2 = latest[1] - current[1];
+    const v3 = latest[2] - current[2];
+    if (v1) return v1 > 0;
+    if (v2) return v2 > 0;
+    if (v3) return v3 > 0;
+    return false;
+}
+
 async function main() {
     const ui = new UI();
+    ui.version.checkUpdate();
 
     const files = await getFiles();
     for (const filename of files) {
